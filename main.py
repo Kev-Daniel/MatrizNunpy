7b3fc92622150ca0c54ff1f4d684d299dbf34ce9
Chriss78Vera <85882973+Chriss78Vera@users.noreply.github.com>
2021-06-14 15:39:35 +0000

    Creacion segunda matriz

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ fila=int(input("Ingrese el numero de filas: " ))
 columna1 = int(input("Ingrese numero de columnas: "))
 columna2= int(input("Ingrese numero de columnas 2: "))
 
-#Serian de dos diga? para la matriz 1 y 2 o no?
-#Multiplicacion de dos matrices pues pa
-#yaya mijo pero no me pegues ya 
-#entonces que hacemos ajajaja BUeno a ver arreglemos eso de las columnas con eso entonces
+#PRIMERA MATRIZ PEDIDA POR EL USUSARIO
 M1=[]
 print("Creacion de su primera matriz: ")
 for i in range (fila):
   M1.append ([0]*columna1)
 for i in range(fila):
   print(M1[i])
+  
+#SEGUNDA MATRIZ PREDIDA POR EL USUSARIO
 M2=[]
 print("Creacion de su segunda Matriz: ")
 for i in range (fila):
