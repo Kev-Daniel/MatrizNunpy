0de500df5b868e90a96bd17e9230507bfa21b3df
Chriss78Vera <85882973+Chriss78Vera@users.noreply.github.com>
2021-06-14 15:38:09 +0000

    Creacion primera matriz

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import numpy as np
+print("que no mas se hace muchachos")
+print("Aver ya hagamos")
+fila=int(input("Ingrese el numero de filas: " ))
+columna1 = int(input("Ingrese numero de columnas: "))
+columna2= int(input("Ingrese numero de columnas 2: "))
 
-
-
+#Serian de dos diga? para la matriz 1 y 2 o no?
+#Multiplicacion de dos matrices pues pa
+#yaya mijo pero no me pegues ya 
+#entonces que hacemos ajajaja BUeno a ver arreglemos eso de las columnas con eso entonces
+M1=[]
+print("Creacion de su primera matriz: ")
+for i in range (fila):
+  M1.append ([0]*columna1)
+for i in range(fila):
+  print(M1[i])
+M2=[]
+print("Creacion de su segunda Matriz: ")
+for i in range (fila):
+  M2.append ([0]*columna2)
+for i in range (fila):
+  print(M2[i])
