022a91509194d0356e26b41cfc25b17eee4c5958
Chriss78Vera <85882973+Chriss78Vera@users.noreply.github.com>
2021-06-15 17:47:44 +0000

    Solucion de la matriz con la columna y fila

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ print("----- MULTIPLICACION DE MATRIZ -----")
 print("Matriz 1")
 fila=int(input("Ingrese el numero de filas: " ))
 columna1 = int(input("Ingrese numero de columnas: "))
+columna2= int(input("Ingrese numero de columnas: "))
 print("--------------------------------------------- ")
 M1=np.zeros((fila, columna1))
 print(M1)
 #Matriz N°2
 print("--------------------------------------------- ")
 print("Matriz 2")
-fila2=int(input("Ingrese el numero de filas: " ))
-columna2= int(input("Ingrese numero de columnas: "))
 print("--------------------------------------------- ")
-M2= np.zeros((fila2,columna2))
+M2= np.zeros((columna2, fila))
 print(M2)
 print("--------------------------------------------- ")
 #Ingreso de valores de ususarios
@@ -30,11 +29,9 @@ print("--------------------------------------------- ")
 #Ingreso de valores para la segunda Matriz
 print("Ingreso de valores de la segunda matriz: ")
 #For para el ingreso de valores en la filas y en la columna remplazando el 0 por un valor ingresado por el ususario
-for i in range (fila2):
-  for j in range (columna2):
+for i in range (columna2):
+  for j in range (fila):
     M2[i][j]=float(input("Introduce el valor de (%d, %d):" %(i,j)))
 print("--------------------------------------------- ")   
 print(M2)
 print("--------------------------------------------- ")
-#Multiplicacion
-
