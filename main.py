8b57dc46a77baf1d1fcfb169ee0e93293ae18cf7
Chriss78Vera <85882973+Chriss78Vera@users.noreply.github.com>
2021-06-15 17:33:27 +0000

    Ingreso de valores por el usuario

--- main.py
+++ main.py
@@ -18,7 +18,23 @@ M2= np.zeros((fila2,columna2))
 print(M2)
 print("--------------------------------------------- ")
 #Ingreso de valores de ususarios
-#Multiplicacion de Matrices
-
-
+#Ingreso de valores primera matriz
+print("Ingreso de valores de la primera matriz: ")
+#For para el ingreso de valores en la filas y en la columna remplazando el 0 por un valor ingresado por el ususario
+for i in range (fila):
+  for j in range (columna1):
+    M1[i][j]=float(input("Introduce el valor de (%d, %d):" %(i,j)))
+print("--------------------------------------------- ")   
+print(M1)
+print("--------------------------------------------- ")
+#Ingreso de valores para la segunda Matriz
+print("Ingreso de valores de la segunda matriz: ")
+#For para el ingreso de valores en la filas y en la columna remplazando el 0 por un valor ingresado por el ususario
+for i in range (fila2):
+  for j in range (columna2):
+    M2[i][j]=float(input("Introduce el valor de (%d, %d):" %(i,j)))
+print("--------------------------------------------- ")   
+print(M2)
+print("--------------------------------------------- ")
+#Multiplicacion
 
