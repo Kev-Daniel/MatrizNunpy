01fa287d9b75919f2eed6f932e1be53a5fed7187
Lizbeth Garcia <66501578+Lizg19@users.noreply.github.com>
2021-06-15 18:11:48 +0000

    Nueva Corrección

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # MatrizNunpy
-Integrantes
-Mateo Vera
-Lizbeth Garcia
-Kevin Veliz
-Kevin Pulupa
+Aqui si ya podemos modificar el programa
+jajaa perdon <3
+
+Aquí agregamos código por si las dudas
+
+Aver probar
--- main.py
+++ main.py
@@ -5,15 +5,16 @@ print("----- MULTIPLICACION DE MATRIZ -----")
 print("Matriz 1")
 fila=int(input("Ingrese el numero de filas: " ))
 columna1 = int(input("Ingrese numero de columnas: "))
-columna2= int(input("Ingrese numero de columnas: "))
+print("Matriz 2")
 print("--------------------------------------------- ")
 M1=np.zeros((fila, columna1))
 print(M1)
 #Matriz N°2
 print("--------------------------------------------- ")
 print("Matriz 2")
+columna2= int(input("Ingrese numero de columnas: "))
 print("--------------------------------------------- ")
-M2= np.zeros((columna2, fila))
+M2= np.zeros((columna1,columna2))
 print(M2)
 print("--------------------------------------------- ")
 #Ingreso de valores de ususarios
@@ -29,9 +30,13 @@ print("--------------------------------------------- ")
 #Ingreso de valores para la segunda Matriz
 print("Ingreso de valores de la segunda matriz: ")
 #For para el ingreso de valores en la filas y en la columna remplazando el 0 por un valor ingresado por el ususario
-for i in range (columna2):
-  for j in range (fila):
+for i in range (columna1):
+  for j in range (columna2):
     M2[i][j]=float(input("Introduce el valor de (%d, %d):" %(i,j)))
 print("--------------------------------------------- ")   
 print(M2)
 print("--------------------------------------------- ")
+
+resultado= np.dot(M1,M2)
+print ("MATRIZ RESULTANTE")
+print(resultado)
